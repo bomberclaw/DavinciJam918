9058ff59c007c64c5462bc7422969acd49fa91ef
White Lion <diegorosas.wl@gmail.com>
2018-09-09 16:30:26 -0300

    asd

--- DavinciJam918-unity/DavinciJam918/Assets/Code/GameManager.cs
+++ DavinciJam918-unity/DavinciJam918/Assets/Code/GameManager.cs
@@ -209,7 +209,7 @@ public class GameManager : Singleton<GameManager> {
 
             for (int i = 0; i < q.requiredQuestionIds.Length; i++)
             {
-                if (!questionsAsked.Contains(q.requiredQuestionIds[i]))
+                if (questionsAsked.Contains(q.requiredQuestionIds[i]))
                     founded = true;
             }
 
