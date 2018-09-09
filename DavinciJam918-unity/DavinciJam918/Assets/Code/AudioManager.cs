cc9a0b3926d7a492bc353c3c38f6929198d93e70
Sirevanac <sirevanacms@gmail.com>
2018-09-09 15:58:56 -0300

    added music logic, final states of music

---
+++ DavinciJam918-unity/DavinciJam918/Assets/Code/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+
+    private FMOD.Studio.EventInstance music;
+    private FMOD.Studio.ParameterInstance whichMusic;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
